9e1103484c02575beb4f0cfb62da10920727d28c
aula <brunogatti@usp.br>
2023-08-31 13:45:26 -0300

    added files

---
+++ python1_problemset.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+print('Olá, Piracicaba')
+name = 'Bruno'
+print('Meu nome é', name)
+color = 'transparente'
+print('Minha cor favorita é', color)
+favActivity = 'dormir'
+print('Minha atividade favorita é', favActivity)
+animal = 'dodô'
+print('Meu animal favorito é', animal)
+
+import sys
+var1 = sys.argv[1]
+var2 = sys.argv[2]
+var3 = sys.argv[3]
+var4 = sys.argv[4]
+print('Meu nome é' + var1 + 'e minha cor favorita é' + var2 + 'o que eu mais gosto de fazer é' + var3 + 'e meu animal favorito é o' + var4)
+
