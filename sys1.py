1e192462f4d306b91710b763eb83476efc4181f3
aula <brunogatti@usp.br>
2023-08-31 13:06:26 -0300

    added file

---
+++ sys1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+friend1 = sys.argv[1] # get first command line parameter
+friend2 = sys.argv[2] # get second command line parameter
+friend3 = sys.argv[3] # get third command line parameter
+# now print a message to the screen
+print(friend1, 'and', friend2, 'are friends, but not', friend3)
+
