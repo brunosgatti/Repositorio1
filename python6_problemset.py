ae3a6da319a34aa4dde483af779047c6eebc96bd
brunosgatti <brunogatti@usp.br>
2023-10-26 10:22:55 -0300

    added file

---
+++ python6_problemset.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+
+#####Q1/2
+with open ('Python_06.txt', 'r') as data_file:
+    with open('Python_06_uc.txt', 'a') as output_file:
+        for line in data_file:
+            output_file.write(line.upper())
+
+with open('Python_06_uc.txt', 'r') as file_object:
+    content = file_object.read()
+
+print(content)
+
+
+
+#####Q1/2
+
+
+#####Q3
+with open('Python_06.seq.txt', 'r') as seq_file:
+    for
+
+#####Q3
+
+
+#####Q4
+with open('Python_06.fastq','r'):
+    
+
